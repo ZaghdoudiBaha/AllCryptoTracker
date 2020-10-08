b7cfc3ebb32a78c8e59408b00add250e8bfa05ea
ZaghdoudiBaha <zaghdoudibaha@gmail.com>
2020-10-08 23:20:51 +0100

    first commit

--- src/app/currencies/bitcoin/bitcoin.page.spec.ts
+++ src/app/currencies/bitcoin/bitcoin.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { BitcoinPage } from './bitcoin.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('BitcoinPage', () => {
+  let component: BitcoinPage;
+  let fixture: ComponentFixture<BitcoinPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ BitcoinPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(BitcoinPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
